1805e455263d98d142bcc611e2150d537f95fd28
BrainClouder <celso.w.dias.a@gmail.com>
2020-02-29 09:02:14 -0300

    first commit on this dude

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, Image, StyleSheet, Text, View, ScrollView } from "react-native";
+import { Menu as MenuIcon,
+LinkedIn as LnLogo,
+GitHub as GHLogo,
+} from '@material-ui/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Link = (props: any) => (
+  <Text
+    {...props}
+    accessibilityRole="link"
+    style={StyleSheet.compose(
+      styles.link,
+      props.style
+    )}
+  />
+);
+
+
+
+class App extends Component {
+  render() {
+    return (<>    
+      <View style={styles.appBar}>
+        <View style={styles.appToolBar}>
+        <Text>Sideboard Menu</Text>
+        <Text>This is some logo</Text>
+        
+        <Text>#</Text>
+        </View>
+      </View>
+      <ScrollView>
+      <View style={styles.app}>
+        <Text>dsddfdf</Text>
+        <Text>Oh yeah baby I'm in React Native now</Text>
+        <Button onPress={() => console.log('hello')} title="ex-ampler" />
+      </View>
+      </ScrollView>
+      <View style={styles.footBar}>
+        <Link href="https://www.linkedin.com/in/contrateme/">
+          Ln
+        </Link>
+        <Link href="https://github.com/BrainClouder">GitHub</Link>
+      </View>
+      </>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  appToolBar: {
+    padding: '0.8em',
+    flex: 1,
+    flexDirection: 'row',
+    flexShrink: 1,
+    width: '80vw',
+    marginLeft: '7vw',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap'
+
+  },
+  appBar: {
+    width: '100vw',
+    height: '3em',
+    backgroundColor: 'steelblue',
+  },
+  footBar: {
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#888888",
+    width: '',
+    left: '',
+  },
+  app: {
+    marginHorizontal: "auto",
+    maxWidth: 500
+  },
+  logo: {
+    height: 80
+  },
+  header: {
+    padding: 20
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  text: {
+    lineHeight: "1.5em",
+    fontSize: "1.125rem",
+    marginVertical: "1em",
+    textAlign: "center"
+  },
+  link: {
+    color: "#1B95E0",
+    textAlign: "center"
+  },
+  code: {
+    fontFamily: "monospace, monospace"
+  }
+});
 
 export default App;
